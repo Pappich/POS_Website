0b5f8c907f509c3e375cf9005c30765de457ceab
sitthatot <sitthatot@gmail.com>
2024-12-18 16:37:50 +0700

    done chart with chartjs

---
+++ src/Components/barChart.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+ChartJS.defaults.font.family = "Noto Sans Thai";
+ChartJS.defaults.font.size = 16;
+ChartJS.defaults.color = "black";
+const BarChart = () => {
+  const data = {
+    labels: [
+      "ม.ค.",
+      "ก.พ.",
+      "มี.ค.",
+      "เม.ย.",
+      "พ.ค.",
+      "มิ.ย.",
+      "ก.ค.",
+      "ส.ค.",
+      "ก.ย.",
+      "ต.ค.",
+      "พ.ย.",
+      "ธ.ค.",
+    ],
+    datasets: [
+      {
+        label: "รายรับทั้งหมด",
+        data: [
+          50000, 10000, 40000, 30000, 60000, 50000, 80000, 70000, 90000, 60000,
+          70000, 40000,
+        ], // Monthly data
+        backgroundColor: "#A3C4DC", // Light blue color
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: true,
+        position: "bottom", // Move the legend to the bottom
+        labels: {
+          usePointStyle: true, // Makes the marker a square or circle
+          pointStyle: "rectRounded", // Use a square-like style
+          padding: 5, // Padding around the legend items
+        },
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false, // Turn off the grid on the x-axis
+        },
+      },
+      y: {
+        beginAtZero: true,
+        ticks: { stepSize: 100 },
+        grid: {
+          display: false, // Turn off the grid on the x-axis
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="w-full h-64">
+      <Bar data={data} options={options} />
+    </div>
+  );
+};
+
+export default BarChart;
--- src/Components/sideBar.jsx
+++ src/Components/sideBar.jsx
@@ -36,7 +36,7 @@ const SideBar = ({ menuTab }) => {
         className="fixed top-0 left-0 z-40 w-50 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-[#DD9F52]">
+        <div className="h-full px-3 py-5 overflow-y-auto bg-[#DD9F52]">
           {/* LOGO */}
           <div className="flex items-center ps-2.5 mb-5">
             <img
