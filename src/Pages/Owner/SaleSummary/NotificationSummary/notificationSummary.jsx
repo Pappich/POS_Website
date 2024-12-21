ee167afcba1d707cb13fbbb080b0185709257b44
sitthatot <sitthatot@gmail.com>
2024-12-21 16:10:29 +0700

    notification Section

--- src/Pages/Owner/SaleSummary/NotificationSummary/notificationSummary.jsx
+++ src/Pages/Owner/SaleSummary/NotificationSummary/notificationSummary.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import SideBar from "../../../../Components/sideBar";
+import NotificationBox from "../../../../Components/ืnotificationBox";
+import { IoMdCart } from "react-icons/io";
+import { IoStatsChart } from "react-icons/io5";
+import { FaBox } from "react-icons/fa";
+import { IoNotifications } from "react-icons/io5";
+import { FaThLarge } from "react-icons/fa";
 
 const NotificationSummary = () => {
-  const myName = [
-    { name: "Tot", age: 5 },
-    { name: "T", age: 51 },
-    { name: "O", age: 52 },
-  ];
-
   return (
     <div>
       <SideBar menuTab={"notificationSummary"} />
-      <h1 className="font-bold">นี่คือหน้า Notification</h1>
+      {/* Sticky Header */}
+      <div className="sticky top-0 bg-white z-10 py-2">
+        <h1 className="font-bold text-xl px-5">แจ้งเตือน</h1>
+      </div>
+      <NotificationBox
+        title="ภาพรวมการขายประจำเดือน มกราคม พ.ศ. 2567 สามารถตรวจสอบได้แล้วในวันนี้"
+        description="หน้าต่าง ภาพรวมการขาย"
+        Icon={FaThLarge}
+        bgColor="bg-[#8DA1AF]"
+      />
+      <NotificationBox
+        title="มีออเดอร์ที่ถูกยกเลิกเข้ามาใหม่ 1 รายการ"
+        description="หน้าต่าง ออเดอร์ทั้งหมด"
+        Icon={IoMdCart}
+        bgColor="bg-[#C7B198]"
+      />
+      <NotificationBox
+        title="ภาพรวมยอดขายในเดือน ธันวาคม พ.ศ. 2567 สามารถตรวจสอบได้แล้วในวันนี้"
+        description="หน้าต่าง ภาพรวมยอดขาย"
+        Icon={IoStatsChart}
+        bgColor="bg-[#DCC894]"
+      />
+      <NotificationBox
+        title="มีสินค้าที่จะใกล้หมดอายุ 1 รายการ กรุณาตรวจสอบให้เรียบร้อย"
+        description="หน้าต่าง คลังสินค้า"
+        Icon={FaBox}
+        bgColor="bg-[#DD9F52]"
+      />
     </div>
   );
 };
