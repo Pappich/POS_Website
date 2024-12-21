34999614d2f7650f3f126c214dc48efc6a41c33f
sitthatot <sitthatot@gmail.com>
2024-12-21 17:00:32 +0700

    make addCategoryButton component

--- src/Pages/Owner/SaleSummary/Stock/stock.jsx
+++ src/Pages/Owner/SaleSummary/Stock/stock.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import SideBar from "../../../../Components/sideBar";
+import AddCategoryButton from "../../../../Components/addCategoryButton";
 
 const Stock = () => {
-  const myName = [
-    { name: "Tot", age: 5 },
-    { name: "T", age: 51 },
-    { name: "O", age: 52 },
-  ];
-
   return (
     <div>
       <SideBar menuTab={"stock"} />
-      <h1 className="font-bold">นี่คือหน้า Stock</h1>
+      <h1 className="font-bold text-xl">คลังสินค้า</h1>
+      <div className="flex justify-between">
+        <span>หมวดหมู่</span>
+        <AddCategoryButton/>
+      </div>
     </div>
   );
 };
