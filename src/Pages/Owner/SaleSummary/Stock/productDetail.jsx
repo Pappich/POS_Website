50f02801d300fb37aa10b190da0716c3ed4772a6
sitthatot <sitthatot@gmail.com>
2024-12-22 17:29:49 +0700

    product detail

--- src/Pages/Owner/SaleSummary/Stock/productDetail.jsx
+++ src/Pages/Owner/SaleSummary/Stock/productDetail.jsx
@@ -29,26 +29,76 @@ const ProductDetail = () => {
   ];
   const unitOptions = ["Unit 1", "Unit 2", "Unit 3", "Unit 4"];
   const volumeUnitOptions = ["ml", "L", "g", "kg"];
-
+  const [stockData, setStockData] = useState([
+    {
+      id: 1,
+      name: "ชาสตอเบอร์รี่",
+      choice: "หวานน้อย",
+      volume: "25 ML",
+      group: "ชาผลไม้",
+    },
+    {
+      id: 2,
+      name: "ชาสตอเบอร์รี่",
+      choice: "หวานปานกลาง",
+      volume: "30 ML",
+      group: "ชาผลไม้",
+    },
+    {
+      id: 3,
+      name: "ชาสตอเบอร์รี่",
+      choice: "หวานมาก",
+      volume: "40 ML",
+      group: "ชาผลไม้",
+    },
+    {
+      id: 4,
+      name: "สตอเบอร์รี่โซดา",
+      choice: "หวานน้อย",
+      volume: "30 ML",
+      group: "โซดาสุดซ่า",
+    },
+    {
+      id: 5,
+      name: "สตอเบอร์รี่โซดา",
+      choice: "หวานปานกลาง",
+      volume: "35 ML",
+      group: "โซดาสุดซ่า",
+    },
+    {
+      id: 6,
+      name: "สตอเบอร์รี่โซดา",
+      choice: "หวานมาก",
+      volume: "45 ML",
+      group: "โซดาสุดซ่า",
+    },
+    {
+      id: 7,
+      name: "ชานมสตอเบอร์รี่",
+      choice: "หวานน้อย",
+      volume: "20 ML",
+      group: "ชานม",
+    },
+    {
+      id: 8,
+      name: "ชานมสตอเบอร์รี่",
+      choice: "หวานปานกลาง",
+      volume: "25 ML",
+      group: "ชานม",
+    },
+    {
+      id: 9,
+      name: "ชานมสตอเบอร์รี่",
+      choice: "หวานมาก",
+      volume: "30 ML",
+      group: "ชานม",
+    },
+  ]);
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
     if (file) {
       setProductImage(URL.createObjectURL(file));
     }
-  };
-
-  const handleSave = () => {
-    if (
-      !productImage ||
-      !productName.trim() ||
-      !productAmount.trim() ||
-      !categoryOption ||
-      !volumeUnit
-    ) {
-      alert("กรุณากรอกข้อมูลให้ครบถ้วน");
-      return;
-    }
-    setIsModalOpen(true);
   };
 
   const closeModal = () => {
@@ -110,8 +160,8 @@ const ProductDetail = () => {
               type="text"
               value={productName}
               onChange={(e) => setProductName(e.target.value)}
-              placeholder="กรอกชื่อสินค้า"
-              className="w-full border border-[#D4B28C] rounded-full p-3 text-gray-600 focus:outline-none focus:ring-2 focus:ring-brown-400"
+              placeholder="น้ำสตรอว์เบอร์รี่ เข้มข้น สูตร 2 "
+              className="w-full bg-[#ECECEC] border border-[#D4B28C] rounded-full p-3 text-gray-600 focus:outline-none focus:ring-2 focus:ring-brown-400"
             />
             <div className="py-2">
               <span className="font-bold">จำนวน</span>
@@ -120,9 +170,22 @@ const ProductDetail = () => {
               type="text"
               value={productAmount}
               onChange={(e) => setProductAmount(e.target.value)}
-              placeholder="กรอกจำนวนของสินค้า"
-              className="w-full border border-[#D4B28C] rounded-full p-3 text-gray-600 focus:outline-none focus:ring-2 focus:ring-brown-400"
+              placeholder="1 ขวด"
+              className="w-full bg-[#ECECEC] border border-[#D4B28C] rounded-full p-3 text-gray-600 focus:outline-none focus:ring-2 focus:ring-brown-400"
             />
+            <div className="py-2">
+              <span className="font-bold">การตัดคลังสินค้า</span>
+            </div>
+            <div className="flex items-center">
+              <div className="text-nowrap pr-5">ปริมาณสุทธิต่อหน่วย</div>
+              <input
+                type="text"
+                value={productAmount}
+                onChange={(e) => setProductAmount(e.target.value)}
+                placeholder="760 มิลลิลิตร"
+                className="w-full bg-[#ECECEC] border border-[#D4B28C] rounded-full p-3 text-gray-600 focus:outline-none focus:ring-2 focus:ring-brown-400"
+              />
+            </div>
           </div>
           <div className="w-full">
             <div className="py-2">
@@ -133,9 +196,9 @@ const ProductDetail = () => {
                 onClick={() =>
                   setIsCategoryDropdownOpen(!isCategoryDropdownOpen)
                 }
-                className="border border-[#D4B28C] rounded-full p-3 text-gray-600 cursor-pointer flex items-center justify-between"
+                className="border bg-[#ECECEC] border-[#D4B28C] rounded-full p-3 text-gray-600 cursor-pointer flex items-center justify-between"
               >
-                {categoryOption || "เลือกตัวเลือก"}
+                {categoryOption || "น้ำชง"}
                 <IoIosArrowDown />
               </div>
               {isCategoryDropdownOpen && (
@@ -147,7 +210,7 @@ const ProductDetail = () => {
                         setCategoryOption(option);
                         setIsCategoryDropdownOpen(false);
                       }}
-                      className="p-3 hover:bg-[#F3E5D8] cursor-pointer text-gray-600"
+                      className="p-3  hover:bg-[#F3E5D8] cursor-pointer text-gray-600"
                     >
                       {option}
                     </div>
@@ -176,18 +239,63 @@ const ProductDetail = () => {
           </div>
         )}
         {/* Save and Back Buttons */}
+
+        <div className="py-2">
+          <span className="font-bold">อัตราส่วนในแต่ละเมนู</span>
+        </div>
+        {/* Table Section */}
+        <div className="overflow-x-auto border rounded-lg p-5">
+          <table className="border-collapse table-auto w-full">
+            <thead>
+              <tr>
+                <th className="py-2 pr-5 text-center border-b border-[#000000]">
+                  ลำดับที่
+                </th>
+                <th className="py-2 text-left border-b border-[#000000]">
+                  รายการสินค้า
+                </th>
+                <th className="px-1 py-2 border-b border-[#000000]">
+                  ตัวเลือก
+                </th>
+                <th className="pl-10 py-2 border-b border-[#000000]">
+                  อัตราส่วน
+                </th>
+                <th className="pl-16 pr-5 py-2 border-b border-[#000000]">
+                  กลุ่ม
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {stockData.map((item) => (
+                <tr key={item.id}>
+                  <td className="pr-5 text-center border-b border-[#F1F4F7]">
+                    {item.id}
+                  </td>
+                  <td className="py-2 break-words border-b border-[#F1F4F7]">
+                    {item.name}
+                  </td>
+                  <td className="py-2 text-center border-b border-[#F1F4F7]">
+                    {item.choice}
+                  </td>
+                  <td className="py-2 pl-10 text-center border-b border-[#F1F4F7]">
+                    {item.volume}
+                  </td>
+                  <td className="py-2 pl-9 text-center border-b border-[#F1F4F7]">
+                    <div className="border border-[#70AB8E] rounded-full text-[#70AB8E] inline-flex items-center justify-center px-2">
+                      {item.group}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <div className="flex mt-8 justify-between">
           <button
             className="px-6 py-3 w-[250px] border rounded-full text-[#D4B28C] border-[#D4B28C] hover:bg-[#f5e9dc] transition-colors font-bold"
             onClick={handleBack}
           >
             ย้อนกลับ
-          </button>
-          <button
-            className="px-6 py-3 w-[250px] bg-[#D4B28C] text-white rounded-full hover:bg-[#cda777] transition-colors font-bold"
-            onClick={handleSave}
-          >
-            บันทึก
           </button>
         </div>
       </div>
